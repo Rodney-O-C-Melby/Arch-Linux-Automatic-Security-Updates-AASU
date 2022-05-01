7d86aea5246df333f880cf2ccbc5975d2d59a6a7
Rodney-O-C-Melby <rodmelby@outlook.com>
2022-05-01 22:13:47 +0100

    Added removal of systemd timers

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo systemctl stop aasu.timer  # stop service
 sudo systemctl disable aasu.timer  # disable service
-sudo rm -rf /usr/bin/aasu  # remove program from path location for clean uninstall
-sudo rm -rf /etc/systemd/system/aasu.*  # remove service from systemd for clean uninstall
-sudo rm -rf /usr/lib/systemd/system/aasu.*
-sudo rm -rf /etc/systemd/system/multi-user.target.wants/aasu.*
+sudo rm -f /usr/bin/aasu  # remove program from path location for clean uninstall
+sudo rm -f /etc/systemd/system/aasu.*  # remove service from systemd for clean uninstall
+sudo rm -f /usr/lib/systemd/system/aasu.*
+sudo rm -f /etc/systemd/system/multi-user.target.wants/aasu.*
+sudo rm -f /etc/systemd/system/timers.target.wants/aasu.*
 echo "Successfully uninstalled Arch-Linux Automatic Security Updates (AASU)."
