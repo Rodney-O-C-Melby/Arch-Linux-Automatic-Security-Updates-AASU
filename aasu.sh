145cb782193342754339d0298e8a3350267f3c11
Rodney O C Melby <zdac122@live.rhul.ac.uk>
2022-05-01 19:42:42 +0100

    Update aasu.sh

--- aasu.sh
+++ aasu.sh
@@ -3,14 +3,14 @@
 # update arch linux system packages, piping output to null, with info, and success/error messages.
 echo Updating System Packages ...
 if sudo pacman -Syu --noconfirm > /dev/null ; then
-    echo "Updated :-)"
+    echo "Updated system packages :-)"
 else
     echo "Error: failed to update or upgrade pacman system packages"
 fi
 # upgrade out of date pip packages, piping output to null, with info, and success/error messages.
 echo Updating Pythons PIP Packages ...
 if pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U > /dev/null ; then
-    echo "Updated :-)"
+    echo "Updated python pip packages :-)"
 else
     echo "Error: failed to update python pip packages"
 fi
